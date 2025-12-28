4792c95504b3028b4cd42b968c36de710c78c733
astra-the-boop <astra.the.boop@icloud.com>
2025-12-28 09:06:12 +0700

    BLEGH /pos

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,92 @@
 mod sky;
 
-use ignore::WalkBuilder;
-use std::fs::File;
-use std::io::{self,Write};
-use std::path::Path;
-use std::path::PathBuf;
+use bsky_sdk::BskyAgent;
+use chrono::Utc;
+use dotenv::dotenv;
+use std::env;
+use std::error::Error;
+use atrium_api::app::bsky::feed::post::RecordData;
+use atrium_api::types::string::Datetime;
 
-fn scanFiles(root: &str) -> Vec<PathBuf>{
-    let mut files = Vec::new();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+    let appPass = env::var("APP_PASS")?;
+    let username = env::var("USERNAME")?;
 
-    let walker = WalkBuilder::new(root).hidden(false).follow_links(false).git_exclude(false).git_global(false).git_ignore(false).build();
+    let agent = BskyAgent::builder().build().await?;
 
-    for item in walker{
-        if let Ok(item) = item{
-            let path = item.path();
+    agent.login(&username, &appPass).await?;
 
-            if path.file_name().and_then(|n| n.to_str()) == Some(".env"){
-                files.push(path.to_path_buf());
-            }
-        }
-    }
+    let post = RecordData{
+        created_at: Datetime::now(),
+        embed: None,
+        entities: None,
+        facets: None,
+        labels: None,
+        langs: None,
+        reply: None,
+        tags: None,
+        text: "Hello, world!".to_string(),
+    };
 
-    files
-}
-
-fn mergeFiles(files: &[std::path::PathBuf], output: &Path) -> io::Result<()>{
-    let mut output = File::create(output)?;
-
-    for path in files{
-        if let Ok(content) = std::fs::read_to_string(path){
-            writeln!(output, "[ {} ]", path.display())?;
-            writeln!(output, "{}", content)?;
-            writeln!(output)?;
-        }
-    }
+    agent.create_record(post).await?;
 
     Ok(())
 }
 
-fn main(){
-    let files = scanFiles("/Users/astra.celestine");
-    println!("{:?}", &files);
 
-    mergeFiles(&files, &Path::new("merged.txt")).unwrap();
-}
+// fn main(){
+//     dotenv().ok();
+//
+//     let appPass = env::var("APP_PASS").expect("APP_PASS is not set");
+//     let username = env::var("USERNAME").expect("USERNAME is not set");
+// }
+
+
+// mod sky;
+//
+// use ignore::WalkBuilder;
+// use std::fs::File;
+// use std::io::{self,Write};
+// use std::path::Path;
+// use std::path::PathBuf;
+//
+// fn scanFiles(root: &str) -> Vec<PathBuf>{
+//     let mut files = Vec::new();
+//
+//     let walker = WalkBuilder::new(root).hidden(false).follow_links(false).git_exclude(false).git_global(false).git_ignore(false).build();
+//
+//     for item in walker{
+//         if let Ok(item) = item{
+//             let path = item.path();
+//
+//             if path.file_name().and_then(|n| n.to_str()) == Some(".env"){
+//                 files.push(path.to_path_buf());
+//             }
+//         }
+//     }
+//
+//     files
+// }
+//
+// fn mergeFiles(files: &[std::path::PathBuf], output: &Path) -> io::Result<()>{
+//     let mut output = File::create(output)?;
+//
+//     for path in files{
+//         if let Ok(content) = std::fs::read_to_string(path){
+//             writeln!(output, "[ {} ]", path.display())?;
+//             writeln!(output, "{}", content)?;
+//             writeln!(output)?;
+//         }
+//     }
+//
+//     Ok(())
+// }
+//
+// fn main(){
+//     let files = scanFiles("/Users/astra.celestine");
+//     println!("{:?}", &files);
+//
+//     mergeFiles(&files, &Path::new("merged.txt")).unwrap();
+// }
--- src/sky.rs
+++ src/sky.rs
@@ -4,6 +4,7 @@ use dotenv::dotenv;
 use std::env;
 use std::error::Error;
 use atrium_api::app::bsky::feed::post::RecordData;
+use atrium_api::types::string::Datetime;
 
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
@@ -15,17 +16,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     agent.login(&username, &appPass).await?;
 
     let post = RecordData{
-        created_at: Some(Utc::now().to_rfc3339()),
+        created_at: Datetime::now(),
         embed: None,
         entities: None,
         facets: None,
+        labels: None,
+        langs: None,
         reply: None,
+        tags: None,
         text: "Hello, world!".to_string(),
     };
 
     agent.create_record(post).await?;
 
-    Ok(());
+    Ok(())
 }
 
 
