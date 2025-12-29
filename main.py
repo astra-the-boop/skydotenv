8c6904028b77d0881eb3f0b403b6ed2bad836137
astra-the-boop <astra.the.boop@icloud.com>
2025-12-29 08:12:45 +0700

    grrrrrrrrr teehee

--- main.py
+++ main.py
@@ -40,17 +40,20 @@ def epsteinFiles(paths):
                 res += "\n"
         res += "\n\n\n"
 
-    print(res)
+    return res
 
 def postTheThingy(grrr):
+    requests.post(
+        "http://localhost:6967/post",
+        headers={
+            "x-api-key": "a9a4ae141698274a3a601afdbc4d028a3ef971e823a3b0eeb0ec3616c16d3d10",
+            "Content-Type": "application/json",
+        },
+        json={"text": grrr}
+    )
 
-
-
-# requests.post(
-#     "http://localhost:6967/post",
-#     headers={
-#         "x-api-key": "a9a4ae141698274a3a601afdbc4d028a3ef971e823a3b0eeb0ec3616c16d3d10",
-#         "Content-Type": "application/json",
-#     },
-#     json={"text": input("enter something to post?")}
-# )
+if input("Hey are you really sure you wanna do this? [y/n]").lower() == "y":
+    if input("You know that this will post your env files publicly for millions to see right? [yes/n]").lower() == "yes":
+        if input("This is really important stuff! Please make sure you know this [I understand/n]") == "I understand":
+            if input("Last warning! [I understand and fully consent to a censored version of the contents of all the files with the name '.env' to be sent and posted publicly online on the social media site Bluesky under the handle @skydotenv.bsky.social/n]") == "I understand and fully consent to a censored version of the contents of all the files with the name '.env' to be sent and posted publicly online on the social media site Bluesky under the handle @skydotenv.bsky.social":
+                postTheThingy(epsteinFiles(searchFiles("/", ".env")))
